78fb2ac88d2dac7f9da8aeaad2a72acfc2e44681
SeniorFluffie <michael.kazman@gmail.com>
2021-03-29 23:04:50 -0400

    updated main function pipeline / removed debug prints

--- converter/imageconverter.py
+++ converter/imageconverter.py
@@ -1,7 +1,4 @@
 
 class ImageConverter(object):
-    def __init__(self):
-        print('Converter Initialized!')
-
     def convert(self, image):
         return image.flatten().tobytes()[:32]
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import warnings
-
 import numpy as np
 
 from converter.convert import convert_image
@@ -7,16 +6,26 @@ from scrambler.scramble import scramble_images
 from scraper.scrape import get_images
 from utils.utils import xor_images, calculate_entropy, verify_entropy, send_warnings
 
+import os, cv2
 
 def main():
     # get images from image scraper
-    images = get_images()
+    #images = get_images()
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    images = [
+        cv2.imread(f'{dir_path}/images/lasvegas_cam.png'), 
+        cv2.imread(f'{dir_path}/images/shibuya_cam.png'), 
+        cv2.imread(f'{dir_path}/images/kusatsu_onsen_cam.png'), 
+    ]
 
     # shuffle image order
     np.random.shuffle(images)
 
     # scramble images
     scrambled_images = scramble_images(images)
+    cv2.imwrite(f'{dir_path}/scrambler/example_photos/lasvegas_cam_shuffled.png', scrambled_images[0])
+    cv2.imwrite(f'{dir_path}/scrambler/example_photos/shibuya_cam_shuffled.png', scrambled_images[1])
+    cv2.imwrite(f'{dir_path}/scrambler/example_photos/kusatsu_onsen_cam_shuffled.png', scrambled_images[2])
 
     # xor scrambled images
     xored_image = xor_images(scrambled_images)
@@ -33,6 +42,11 @@ def main():
     # convert image
     converted_image = convert_image(xored_image)
 
+    print(converted_image)
+
+    # return random bytes
+    return convert_image
+
 
 if __name__ == '__main__':
-    main()
+    print(main())
