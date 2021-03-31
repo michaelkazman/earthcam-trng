55474ea00644c882b0b4c6f226b9f09a2e28faeb
Trevor Johns <slegos@live.ca>
2021-03-31 02:27:31 -0400

    cleanup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import warnings
 import numpy as np
-import os
 import cv2
+from os import urandom
 from converter.convert import convert_image
 from scrambler.scramble import scramble_images
 from scraper.scrape import get_images
@@ -9,22 +9,13 @@ from utils.utils import xor_images, calculate_entropy, verify_entropy, send_warn
 
 def main():
     # get images from image scraper
-    #images = get_images()
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    images = [
-        cv2.imread(f'{dir_path}/images/lasvegas_cam.png'), 
-        cv2.imread(f'{dir_path}/images/shibuya_cam.png'), 
-        cv2.imread(f'{dir_path}/images/kusatsu_onsen_cam.png'), 
-    ]
+    images = get_images()
 
     # shuffle image order
     np.random.shuffle(images)
 
     # scramble images
     scrambled_images = scramble_images(images)
-    cv2.imwrite(f'{dir_path}/scrambler/example_photos/lasvegas_cam_shuffled.png', scrambled_images[0])
-    cv2.imwrite(f'{dir_path}/scrambler/example_photos/shibuya_cam_shuffled.png', scrambled_images[1])
-    cv2.imwrite(f'{dir_path}/scrambler/example_photos/kusatsu_onsen_cam_shuffled.png', scrambled_images[2])
 
     # xor scrambled images
     xored_image = xor_images(scrambled_images)
@@ -37,6 +28,7 @@ def main():
     if (not verify_entropy(entropy)):
         warnings.showwarning = send_warnings
         warnings.warn("Not enough entropy, using /dev/urandom fallback!")
+        return os.urandom(32)
 
     # convert image
     converted_image = convert_image(xored_image)
