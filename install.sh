3a8a6b8424614b53c38d6d2ffd169bb72f1feb36
Michael Kazman <35642445+SeniorFluffie@users.noreply.github.com>
2021-04-01 00:08:08 -0400

    updated installer

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/bash
 # get current dir of script
 script_dir="${0%/*}"
 # install required dependencies
-pip3 install -r "$script_dir"/setup/requirements.txt
+pip3 install --no-warn-script-location -r "$script_dir"/setup/requirements.txt
